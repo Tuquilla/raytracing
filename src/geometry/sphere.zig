41e0abaa7e7309c83459f3676f7601e71cd36dcf
Kilian Glaus <kilian.glaus@hotmail.com>
2024-10-12 17:07:17 +0200

    :construction: print closest sphere

--- src/geometry/sphere.zig
+++ src/geometry/sphere.zig
@@ -2,41 +2,105 @@ const Sphere = @import("../datatypes/sphere.zig");
 const std = @import("std");
 const vector_calc = @import("../helpers/vectors.zig");
 
-pub fn drawSphere(image_3D: *[][]u32, length: i64, height: i64, sphere: *const Sphere.Sphere, camera_position: *const [3]i64, focal_distance: i64) void {
+// pub fn drawSphere(image_3D: *[][]u32, length: i64, height: i64, sphere: *const Sphere.Sphere, camera_position: *const [3]i64, focal_distance: i64) void {
+//     for (image_3D.*, 0..) |row, y| {
+//         for (row, 0..) |_, x| {
+//             if (ray(@as(i64, @intCast(x)), @as(i64, @intCast(y)), length, height, sphere, camera_position, focal_distance)) {
+//                 if (image_3D.*[y][x] == 0xffffff) {
+//                     image_3D.*[y][x] = sphere.color;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+pub fn drawSphere2(image_3D: *[][]u32, length: i64, height: i64, spheres: *std.ArrayList(Sphere.Sphere), camera_position: *const [3]i64, focal_distance: i64) void {
     for (image_3D.*, 0..) |row, y| {
         for (row, 0..) |_, x| {
-            if (ray(@as(i64, @intCast(x)), @as(i64, @intCast(y)), length, height, sphere, camera_position, focal_distance)) {
-                image_3D.*[y][x] = sphere.color;
+            const sphere_index = ray2(@as(i64, @intCast(x)), @as(i64, @intCast(y)), length, height, spheres, camera_position, focal_distance);
+            if (sphere_index != -1) {
+                image_3D.*[y][x] = spheres.items[@as(usize, @intCast(sphere_index))].color;
             }
         }
     }
 }
 
-pub fn ray(x: i64, y: i64, length: i64, height: i64, sphere: *const Sphere.Sphere, camera_position: *const [3]i64, focal_distance: i64) bool {
-    const direction = [_]i64{ -1 * @divExact(length, 2) + x, @divExact(height, 2) - y, focal_distance };
-    //std.debug.print("direction {any}\n", .{direction});
-    const a = vector_calc.scalarProduct(&direction, &direction);
-    const w = vector_calc.multiplyNumber(&direction, 2);
-    const vm = vector_calc.subtractVector(camera_position, &sphere.center);
-    const b = vector_calc.scalarProduct(&w, &vm);
-    const c = vector_calc.scalarProduct(&vm, &vm) - std.math.pow(i64, sphere.radius, 2);
-    //std.debug.print("a = {}, b = {}, c = {}", .{ a, b, c });
-    if (rayCollision(a, b, c)) {
-        return true;
+// fn ray(x: i64, y: i64, length: i64, height: i64, sphere: *const Sphere.Sphere, camera_position: *const [3]i64, focal_distance: i64) bool {
+//     const direction = [_]i64{ -1 * @divExact(length, 2) + x, @divExact(height, 2) - y, focal_distance };
+//     //std.debug.print("direction {any}\n", .{direction});
+//     const a = vector_calc.scalarProduct(&direction, &direction);
+//     const w = vector_calc.multiplyNumber(&direction, 2);
+//     const vm = vector_calc.subtractVector(camera_position, &sphere.center);
+//     const b = vector_calc.scalarProduct(&w, &vm);
+//     const c = vector_calc.scalarProduct(&vm, &vm) - std.math.pow(i64, sphere.radius, 2);
+//     //std.debug.print("a = {}, b = {}, c = {}", .{ a, b, c });
+//     if (rayCollision(a, b, c)) {
+//         return true;
+//     }
+//     return false;
+// }
+
+fn ray2(x: i64, y: i64, length: i64, height: i64, spheres: *std.ArrayList(Sphere.Sphere), camera_position: *const [3]i64, focal_distance: i64) i64 {
+    var smallest_t: f64 = std.math.floatMax(f64);
+    var sphere_index: i64 = -1;
+    var index: usize = 0;
+    for (spheres.items) |sphere| {
+        const direction = [_]i64{ -1 * @divExact(length, 2) + x, @divExact(height, 2) - y, focal_distance };
+        const a = vector_calc.scalarProduct(&direction, &direction);
+        const w = vector_calc.multiplyNumber(&direction, 2);
+        const vm = vector_calc.subtractVector(camera_position, &sphere.center);
+        const b = vector_calc.scalarProduct(&w, &vm);
+        const c = vector_calc.scalarProduct(&vm, &vm) - std.math.pow(i64, sphere.radius, 2);
+        const intersection = intersect2(a, b, c);
+        if (intersection < smallest_t and intersection > 0.0) {
+            smallest_t = intersect2(a, b, c);
+            sphere_index = @as(i64, @intCast(index));
+        }
+        index += 1;
     }
-    return false;
+    return sphere_index;
 }
 
-fn rayCollision(a: i64, b: i64, c: i64) bool {
-    if (intersect(a, b, c)) {
-        return true;
+// fn rayCollision(a: i64, b: i64, c: i64) bool {
+//     if (intersect(a, b, c)) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// fn intersect(a: i64, b: i64, c: i64) bool {
+//     if (std.math.pow(i64, b, 2) - 4 * a * c > 0) {
+//         return true;
+//     }
+//     return false;
+// }
+
+fn intersect2(a: i64, b: i64, c: i64) f64 {
+    var t1: f64 = 0;
+    var t2: f64 = 0;
+    const diskriminante = std.math.pow(i64, b, 2) - 4 * a * c;
+    if (diskriminante > 0) {
+        const t1_upper: f64 = @as(f64, @floatFromInt(-b - std.math.sqrt(@as((u64), @intCast(diskriminante)))));
+        const t2_upper: f64 = @as(f64, @floatFromInt(-b + std.math.sqrt(@as((u64), @intCast(diskriminante)))));
+        const lower: f64 = @as(f64, @floatFromInt(2 * a));
+        t1 = t1_upper / lower;
+        t2 = t2_upper / lower;
+        // smallest t is the closest entry point of a ray into the objective
+        if (t1 < t2) {
+            return t1;
+        } else {
+            return t2;
+        }
     }
-    return false;
+    return t1;
 }
 
-fn intersect(a: i64, b: i64, c: i64) bool {
-    if (std.math.pow(i64, b, 2) - 4 * a * c > 0) {
-        return true;
-    }
-    return false;
+test "intersect2" {
+    const a = 1;
+    const b = -5;
+    const c = 6;
+
+    const result = intersect2(a, b, c);
+
+    try std.testing.expectEqual(result, 2.0);
 }
--- src/main.zig
+++ src/main.zig
@@ -14,14 +14,20 @@ pub fn main() !void {
     const CAMERA_UP = [_]i64{ 0, 1, 0 };
     const FOCAL_DISTANCE: i64 = 10;
     const CAMERA_POSITION = [_]i64{ 0, 0, -20 };
-    const SPHERE_CENTRE = [_]i64{ 0, 0, 0 };
+    const SPHERE_CENTRE_1 = [_]i64{ 0, 0, 0 };
+    const SPHERE_CENTRE_2 = [_]i64{ 2, 2, 2 };
+    const SPHERE_CENTRE_3 = [_]i64{ -4, -4, -2 };
     const LENGTH: i64 = 64;
     const HEIGHT: i64 = 48;
-    const RADIUS: i64 = 5;
+    const RADIUS_1: i64 = 5;
+    const RADIUS_2: i64 = 5;
+    const RADIUS_3: i64 = 5;
     const COLOR_SPHERE_1: u32 = 0x00ff00;
+    const COLOR_SPHERE_2: u32 = 0x0D1CEE;
+    const COLOR_SPHERE_3: u32 = 0xFFFF00;
     const HEADER_TYPE = "P6\n";
     const MAX_COLOR = "255\n";
-    const circle = Circle.Circle{ .origin_x = LENGTH / 2, .origin_y = HEIGHT / 2, .radius = RADIUS };
+    const circle = Circle.Circle{ .origin_x = LENGTH / 2, .origin_y = HEIGHT / 2, .radius = RADIUS_1 };
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -59,18 +65,27 @@ pub fn main() !void {
         }
     }
 
-    // fill image with "."
+    // paint white image
     print_image.createImage(&image_3D);
 
     // first sphere in image
-    const sphere = Sphere.Sphere{ .radius = RADIUS, .center = SPHERE_CENTRE, .color = COLOR_SPHERE_1 };
+    const sphere_1 = Sphere.Sphere{ .radius = RADIUS_1, .center = SPHERE_CENTRE_1, .color = COLOR_SPHERE_1 };
+    const sphere_2 = Sphere.Sphere{ .radius = RADIUS_2, .center = SPHERE_CENTRE_2, .color = COLOR_SPHERE_2 };
+    const sphere_3 = Sphere.Sphere{ .radius = RADIUS_3, .center = SPHERE_CENTRE_3, .color = COLOR_SPHERE_3 };
     _ = CAMERA_RIGHT;
     _ = CAMERA_UP;
 
-    // draw sphere in image
-    draw_sphere.drawSphere(&image_3D, LENGTH, HEIGHT, &sphere, &CAMERA_POSITION, FOCAL_DISTANCE);
+    // put spheres in array
+    var spheres = std.ArrayList(Sphere.Sphere).init(allocator);
+    defer spheres.deinit();
+    try spheres.append(sphere_1);
+    try spheres.append(sphere_2);
+    try spheres.append(sphere_3);
 
-    // print image with green color
-    //print_image.drawImage(&image_3D);
+    // draw sphere in image
+    //draw_sphere.drawSphere(&image_3D, LENGTH, HEIGHT, &sphere_1, &CAMERA_POSITION, FOCAL_DISTANCE);
+    //draw_sphere.drawSphere(&image_3D, LENGTH, HEIGHT, &sphere_2, &CAMERA_POSITION, FOCAL_DISTANCE);
+    draw_sphere.drawSphere2(&image_3D, LENGTH, HEIGHT, &spheres, &CAMERA_POSITION, FOCAL_DISTANCE);
+
     try print_image.drawImageAsPPM(&image_3D, LENGTH, HEIGHT, MAX_COLOR, HEADER_TYPE);
 }
