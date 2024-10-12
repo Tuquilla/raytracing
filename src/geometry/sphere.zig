c07dfa46b973444ecd0f2524b097d83d24834fb6
Kilian Glaus <kilian.glaus@hotmail.com>
2024-10-12 17:08:10 +0200

    :fire: Remove commented code

--- src/geometry/sphere.zig
+++ src/geometry/sphere.zig
@@ -2,18 +2,6 @@ const Sphere = @import("../datatypes/sphere.zig");
 const std = @import("std");
 const vector_calc = @import("../helpers/vectors.zig");
 
-// pub fn drawSphere(image_3D: *[][]u32, length: i64, height: i64, sphere: *const Sphere.Sphere, camera_position: *const [3]i64, focal_distance: i64) void {
-//     for (image_3D.*, 0..) |row, y| {
-//         for (row, 0..) |_, x| {
-//             if (ray(@as(i64, @intCast(x)), @as(i64, @intCast(y)), length, height, sphere, camera_position, focal_distance)) {
-//                 if (image_3D.*[y][x] == 0xffffff) {
-//                     image_3D.*[y][x] = sphere.color;
-//                 }
-//             }
-//         }
-//     }
-// }
-
 pub fn drawSphere2(image_3D: *[][]u32, length: i64, height: i64, spheres: *std.ArrayList(Sphere.Sphere), camera_position: *const [3]i64, focal_distance: i64) void {
     for (image_3D.*, 0..) |row, y| {
         for (row, 0..) |_, x| {
@@ -24,21 +12,6 @@ pub fn drawSphere2(image_3D: *[][]u32, length: i64, height: i64, spheres: *std.A
         }
     }
 }
-
-// fn ray(x: i64, y: i64, length: i64, height: i64, sphere: *const Sphere.Sphere, camera_position: *const [3]i64, focal_distance: i64) bool {
-//     const direction = [_]i64{ -1 * @divExact(length, 2) + x, @divExact(height, 2) - y, focal_distance };
-//     //std.debug.print("direction {any}\n", .{direction});
-//     const a = vector_calc.scalarProduct(&direction, &direction);
-//     const w = vector_calc.multiplyNumber(&direction, 2);
-//     const vm = vector_calc.subtractVector(camera_position, &sphere.center);
-//     const b = vector_calc.scalarProduct(&w, &vm);
-//     const c = vector_calc.scalarProduct(&vm, &vm) - std.math.pow(i64, sphere.radius, 2);
-//     //std.debug.print("a = {}, b = {}, c = {}", .{ a, b, c });
-//     if (rayCollision(a, b, c)) {
-//         return true;
-//     }
-//     return false;
-// }
 
 fn ray2(x: i64, y: i64, length: i64, height: i64, spheres: *std.ArrayList(Sphere.Sphere), camera_position: *const [3]i64, focal_distance: i64) i64 {
     var smallest_t: f64 = std.math.floatMax(f64);
@@ -60,20 +33,6 @@ fn ray2(x: i64, y: i64, length: i64, height: i64, spheres: *std.ArrayList(Sphere
     }
     return sphere_index;
 }
-
-// fn rayCollision(a: i64, b: i64, c: i64) bool {
-//     if (intersect(a, b, c)) {
-//         return true;
-//     }
-//     return false;
-// }
-
-// fn intersect(a: i64, b: i64, c: i64) bool {
-//     if (std.math.pow(i64, b, 2) - 4 * a * c > 0) {
-//         return true;
-//     }
-//     return false;
-// }
 
 fn intersect2(a: i64, b: i64, c: i64) f64 {
     var t1: f64 = 0;
